ce58ad192be5898d762cd703865fe10144cb4ac6
Rahul Saha <rahul.saha.c@gmail.com>
2020-10-03 01:08:49 +0530

    Modified profile command output

--- bamboo/command/profile_command.py
+++ bamboo/command/profile_command.py
@@ -5,7 +5,7 @@ from bamboo.utils.printing import print_success
 def run(**_):
     profiles = light_load_sync_profiles()
     for profile in profiles:
-        output = f'{profile.name:16} : {profile.source_device.name:10} ==> {profile.target_device.name:10}'
+        output = f'{profile.name:16} : {profile.source_device.name:10} -> {profile.target_device.name:10}'
         if profile.source_device.is_device_online() and profile.target_device.is_device_online():
             print_success(output)
         else:
