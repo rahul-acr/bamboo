a8c9ce02673e1e930173780888667158cfbd4faf
Rahul Saha <rahul.saha.c@gmail.com>
2020-10-03 17:30:57 +0530

    Added dist wheel Fixed typo of version in 'about'

--- bamboo/command/about_command.py
+++ bamboo/command/about_command.py
@@ -2,6 +2,6 @@ from bamboo.utils.printing import print_bold
 
 
 def run(**_):
-    print_bold('Bamboo Backup 1.0')
+    print_bold('Bamboo Backup 0.1')
     print('Developed by Rahul Saha (rahul.saha.c@gmail.com)')
     print('Github : github.com/rahul-acr')
